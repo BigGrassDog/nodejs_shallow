a3adfa4e2da8c2aaf5b306ac6efccc0e16215aeb
gd <shengxiaoan@icloud.com>
2022-10-11 23:16:39 +0800

    update

--- 009-内置模块-http模块-jsonp/01-jsonp.js
+++ 009-内置模块-http模块-jsonp/01-jsonp.js
@@ -1,11 +1,22 @@
 const http = require('http')
+const url = require('url')
 
 const app = http.createServer()
 
 app.on('request', (req, res) => {
-  
+  const urlobj = url.parse(req.url)
+  switch (urlobj.pathname) {
+    case '/api/aaa':
+      res.end(`grassdog(${JSON.stringify({
+        name: 'dog',
+        age: 18
+      })})`)
+      break
+    default:
+      res.end('404')
+  }
 })
 
-app.listen(3000, () => { 
-  console.log('server start');
+app.listen(3000, () => {
+  console.log('server start')
 })
