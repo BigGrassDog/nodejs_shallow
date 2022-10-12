4c1fdcff743dde089b6342353bdc3614ed8b0e65
huzun <shengxiaoan@icloud.com>
2022-10-12 13:57:27 +0800

    http get

---
+++ 011-内置模块-http模块-get/01.get.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const https = require("https");
+const url = require("url");
+
+const app = http.createServer();
+
+app.on("request", (req, res) => {
+  const urlobj = url.parse(req.url, true);
+
+  res.writeHead(200, {
+    "Content-Type": "application/json;charset=utf-8",
+    // cors头
+    "access-control-allow-origin": "*",
+  });
+
+  switch (urlobj.pathname) {
+    case "/api/aaa":
+      // 客户端 去猫眼要数据
+      httpGet(res);
+      break;
+    default:
+      res.end("404");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server start");
+});
+
+function httpGet(response) {
+  let data = "";
+  https.get(
+    `https://i.maoyan.com/api/mmdb/movie/v3/list/hot.json?ct=%E5%B8%B8%E5%B7%9E&ci=89&channelId=4`,
+    (res) => {
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        console.log(data);
+        response.end(data)
+      });
+    }
+  );
+}
