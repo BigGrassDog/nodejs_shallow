e8229a6e44968584e74f02d1931c1946077e248b
huzun <shengxiaoan@icloud.com>
2022-10-14 10:49:18 +0800

    zlib

---
+++ 017-内置模块-zlib模块/01-gzip.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs");
+const zlib = require("zlib");
+const gzip = zlib.createGzip();
+
+http
+  .createServer((req, res) => {
+    // res 可写流
+    const readStream = fs.createReadStream("./index.js");
+    res.writeHead(200, {
+      "Content-Type": "application/x-javascript;charset=utf-8",
+      "Content-Encoding": "gzip",
+    });
+    // readStream.pipe(res);
+    readStream.pipe(gzip).pipe(res);
+  })
+  .listen(3000, () => {
+    console.log("server start");
+  });
