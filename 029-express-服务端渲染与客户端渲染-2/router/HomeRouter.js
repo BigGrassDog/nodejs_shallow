fc0668cb3e256025d072464e76ac22056b6284fe
huzun <shengxiaoan@icloud.com>
2022-11-04 14:50:17 +0800

    服务端渲染，客户端渲染

--- 029-express-服务端渲染与客户端渲染-2/router/HomeRouter.js
+++ 029-express-服务端渲染与客户端渲染-2/router/HomeRouter.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 // 路由级别
 router.get("/", (req, res) => {
-  res.send("home");
+  // res.send("home");
+
+  const list = ['aaaaa', 'bbbbb', 'cccccc', 'ddddddd']
+  const el = '<b>我是加粗</b>'
+  res.render('home', {list:list,el:el})
 });
 
 router.get("/list", (req, res) => {
--- 029-express-服务端渲染与客户端渲染-2/router/LoginRouter.js
+++ 029-express-服务端渲染与客户端渲染-2/router/LoginRouter.js
@@ -8,15 +8,18 @@ router.get("/", (req, res) => {
   // console.log(req.query);
 
   // 渲染模板后返回给前端
-  res.render('login', {title:'11111111'}) // 找 views 文件夹下的 login.ejs
+  res.render('login', {error:'',title:'登录页面'}) //  找 views 文件夹下的 login.ejs
 });
 
 router.post('/', (req, res) => { 
   console.log(req.body);
   if (req.body.username === 'grassdog' && req.body.password === '123456') {
-    console.log('登录成功');
+    // console.log('登录成功');
+    // 重定向到 home
+    res.redirect('/home')
   } else { 
     console.log('登录失败');
+    res.render('login', {error:'用户名密码不匹配',title:'登录页面'})
   }
 })
 
