49cfae12bb00651b1da7f1c9d89411a6cfba56cf
gd <shengxiaoan@icloud.com>
2022-11-13 14:35:10 +0800

    express 操作nodejs-1

--- code/server/routes/users.js
+++ code/server/routes/users.js
@@ -1,22 +1,31 @@
-var express = require('express');
+var express = require("express");
+const UserModel = require("../model/UserModel");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   // 读取前端的 cookie 值
   // console.log(req.cookies);
 
   // 设置前端的 cookie 值
-  res.cookie('gender','变态')
-  res.send('respond with a resource');
+  res.cookie("gender", "变态");
+  res.send("respond with a resource");
 });
 
-router.post('/user/add', (req, res) => {
+router.post("/user/add", (req, res) => {
   console.log(req.body);
-  // 插入数据库 
-  // 1. 创建一个模型（user,限制filed类型），一一对应数据的集合（users） 
+  // 插入数据库
+  // 1. 创建一个模型（user,限制filed类型），一一对应数据的集合（users）
   // user.create user.find user.delete user.update
-  res.send({ok:1});
- })
+  const { username, password, age } = req.body;
+  UserModel.create({
+    username,
+    password,
+    age,
+  }).then((data) => {
+    console.log(data);
+  });
+  res.send({ ok: 1 });
+});
 
 module.exports = router;
