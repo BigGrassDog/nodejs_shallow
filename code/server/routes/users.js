e40edb708697383ef441728ddff74e7cd80c0ca5
huzun <shengxiaoan@icloud.com>
2022-11-15 16:56:09 +0800

    今天这个批班就上到这吧

--- code/server/routes/users.js
+++ code/server/routes/users.js
@@ -25,17 +25,23 @@ router.post("/user/add", (req, res) => {
     age,
   }).then((data) => {
     console.log(data);
+    res.send({ ok: 1 });
   });
-  res.send({ ok: 1 });
 });
 
 // 动态路由，获取 id
 router.post('/user/update/:id', (req, res) => { 
   console.log(req.body, req.params);
+  const { username, password, age } = req.body;
   UserModel.updateOne({
     _id:req.params.id
+  }, {
+    username,
+    password,
+    age,
+  }).then(data => { 
+    res.send({ ok: 1 });
   })
-  res.send({ ok: 1 });
 })
 
 module.exports = router;
