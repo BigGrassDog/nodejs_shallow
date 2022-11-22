b7e1246e00d96bdbe606899954dd3e4e6803ab5c
huzun <shengxiaoan@icloud.com>
2022-11-22 16:55:29 +0800

    json web token

--- code/server/routes/index.js
+++ code/server/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const JWT = require('../util/JWT')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,5 +12,36 @@ router.get('/', function (req, res, next) {
     }
 
 });
+
+/**
+ * 测试 token 的 加密与验证过程
+ */
+// const jwt = require('jsonwebtoken')
+// const token = jwt.sign({
+//     data: 'biggrassdog'
+// }, 'anydata', {expiresIn: '10s'})
+// console.log('token', token)
+//
+// const decoded = jwt.verify(token, 'anydata')
+// console.log(decoded)
+//
+// // 过期时间 10 秒，10 秒后再解密一次
+// setTimeout(() => {
+//     try {
+//         const decoded = jwt.verify(token, 'anydata')
+//         console.log(decoded)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }, 11000)
+
+const token = JWT.generate({name: 'gd11'}, '10s')
+console.log(JWT.verify(token))
+setTimeout(() => {
+    console.log(JWT.verify(token))
+}, 9000)
+setTimeout(() => {
+    console.log(JWT.verify(token))
+}, 11000)
 
 module.exports = router;
