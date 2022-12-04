29cb9f898bffb7da5a6ad6ea91a535e65e26f843
gd <shengxiaoan@icloud.com>
2022-12-04 22:13:18 +0800

    1

--- code/server_jwt/routes/index.js
+++ code/server_jwt/routes/index.js
@@ -5,11 +5,12 @@ const JWT = require('../util/JWT')
 /* GET home page. */
 router.get('/', function (req, res, next) {
     // 判断 req.session.user
-    if (req.session.user) {
+    console.log('首页',req)
+    // if (req.session.user) {
         res.render('index', {title: 'Express'});
-    } else {
-        res.redirect('/login')
-    }
+    // } else {
+    //     res.redirect('/login')
+    // }
 
 });
 
