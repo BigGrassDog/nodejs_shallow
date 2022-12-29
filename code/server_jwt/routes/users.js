a217eaab9dac71c6c2d5b496e9a24c804c083a3b
bgd <shengxiaoan@icloud.com>
2022-12-29 13:23:52 +0800

    apidoc 配置

--- code/server_jwt/routes/users.js
+++ code/server_jwt/routes/users.js
@@ -49,6 +49,32 @@ router.post("/user", upload.single('avatar'), UserController.addUser);
 router.put("/user/:id", UserController.updateUser);
 
 // 删除用户信息
+/**
+ * @api {delete} /api/user:id 删除用户
+ * @apiName deleteUser
+ * @apiGroup usergroup
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} username 用户名
+ * @apiParam {String} password 密码
+ * @apiParam {Number} age 年龄
+ * @apiParam {File} avatar 头像
+ *
+ * @apiParamExample {multipart/form-data} Request-Example:
+ * {
+ *     username:'gd',
+ *     password:'123',
+ *     age:100,
+ *     avatar:File
+ * }
+ *
+ *
+ * @apiSuccess (200) {Number}  ok 标识成功字段
+ * @apiSuccessExample {type} Success-Response
+ * {
+ *     ok:1
+ * }
+ */
 router.delete("/user/:id", UserController.deleteUser);
 
 // 查询用户信息 分页
