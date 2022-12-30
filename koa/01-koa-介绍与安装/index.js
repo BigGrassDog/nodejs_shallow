fbb2b2d6cae7cf7e16d509a6152330dd1ed029f6
bgd <shengxiaoan@icloud.com>
2022-12-30 13:49:02 +0800

    koa 介绍安装

--- koa/01-koa-介绍与安装/index.js
+++ koa/01-koa-介绍与安装/index.js
@@ -2,6 +2,10 @@ const Koa = require('koa')
 
 const app = new Koa
 
-app.use(()=>{
-
+// ctx => context 上下文
+app.use((ctx,next)=>{
+    // ctx.response
+    ctx.response.body = "hello world"
 })
+
+app.listen(3000)
